249934521cd4e4753d9a2a693400b280fafe6626
Moiwa <samuelmoiwa@gmail.com>
2025-03-26 18:02:24 +0000

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Resources from './pages/Resources';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Shop from './pages/Shop'
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
           <Footer />
     </div>
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div className='w-full h-full flex justify-center items-center flex-col '>
+      <div className='w-full pt-14 flex flex-col justify-center items-center max-w-[110rem]'>
+          
+      </div>
+    </div>
+  )
+}
+
+export default ContactUs
